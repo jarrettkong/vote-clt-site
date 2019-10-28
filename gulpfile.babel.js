d2924576e1a3b41bde538ea41436dc9b315af3f6
Andrew Natoli <4162073+AndrewNatoli@users.noreply.github.com>
2019-10-28 10:21:34 -0400

    feat: Adding base content, more theme changes, removing base template content

---
+++ gulpfile.babel.js
@@ -0,0 +1,173 @@
+/* eslint-env node, process */
+"use strict";
+
+// Gulp and node
+const gulp = require( "gulp" );
+const cp = require( "child_process" );
+const notify = require( "gulp-notify" );
+const size = require( "gulp-size" );
+
+// Basic workflow plugins
+const browserSync = require( "browser-sync" );
+const browserify = require( "browserify" );
+const source = require( "vinyl-source-stream" );
+const buffer = require( "vinyl-buffer" );
+const clean = require( "gulp-clean" );
+const sass = require( "gulp-sass" );
+const jekyll = process.platform === "win32" ? "jekyll.bat" : "jekyll";
+const messages = {
+    jekyllBuild: "<span style=\"color: grey\">Running:</span> $ jekyll build"
+};
+
+// Performance workflow plugins
+const htmlmin = require( "gulp-htmlmin" );
+const prefix = require( "gulp-autoprefixer" );
+const sourcemaps = require( "gulp-sourcemaps" );
+const uglify = require( "gulp-uglify" );
+const critical = require( "critical" );
+
+// Image Generation
+const rename = require( "gulp-rename" );
+
+const src = {
+  css: "_sass/jekyll-sleek.scss",
+  js: "_js/scripts.js"
+};
+const dist = {
+  css: "_site/assets/css",
+  js: "_site/assets/js"
+};
+
+function handleErrors() {
+  var args = Array.prototype.slice.call( arguments );
+  notify.onError( {
+    title: "Compile Error",
+    message: "<%= error.message %>"
+  } ).apply( this, args );
+  this.emit( "end" ); // Keep gulp from hanging on this task
+}
+
+// SASS
+gulp.task( "sass", () => {
+  return gulp.src( src.css )
+    .pipe( sourcemaps.init() )
+    .pipe( sass( {
+      outputStyle: "compressed",
+      includePaths: [ "scss" ],
+      onError: browserSync.notify
+    } ).on( "error", sass.logError ) )
+    .pipe( sourcemaps.write( { includeContent: false } ) )
+    .pipe( sourcemaps.init( { loadMaps: true } ) )
+    .pipe( prefix() )
+    .pipe( sourcemaps.write( "./" ) )
+    .pipe( rename( { basename: "main" } ) )
+    .pipe( gulp.dest( dist.css ) )
+    .pipe( browserSync.reload( { stream: true } ) )
+    .pipe( gulp.dest( "assets/css" ) );
+} );
+
+//  JS
+gulp.task( "js", () => {
+  return browserify( src.js, { debug: true, extensions: [ "es6" ] } )
+    .transform( "babelify", { presets: [ "es2015" ] } )
+    .bundle()
+    .on( "error", handleErrors )
+    .pipe( source( "bundle.js" ) )
+    .pipe( buffer() )
+    .pipe( sourcemaps.init( { loadMaps: true } ) )
+    .pipe( uglify() )
+    .pipe( sourcemaps.write( "./maps" ) )
+    .pipe( size() )
+    .pipe( gulp.dest( dist.js ) )
+    .pipe( browserSync.reload( { stream: true } ) )
+    .pipe( gulp.dest( "assets/js" ) );
+} );
+
+gulp.task( "critical", done => {
+  critical.generate( {
+    base: "_site/",
+    src: "index.html",
+    css: [ "assets/css/main.css" ],
+    dimensions: [ {
+      width: 320,
+      height: 480
+    }, {
+      width: 768,
+      height: 1024
+    }, {
+      width: 1280,
+      height: 960
+    } ],
+    dest: "../_includes/critical.css",
+    minify: true,
+    extract: false,
+    ignore: [ "@font-face" ]
+  } );
+  done();
+} );
+
+// Minify HTML
+gulp.task( "html", done => {
+    gulp.src( "./_site/index.html" )
+      .pipe( htmlmin( { collapseWhitespace: true } ) )
+      .pipe( gulp.dest( "./_site" ) );
+    gulp.src( "./_site/*/*html" )
+      .pipe( htmlmin( { collapseWhitespace: true } ) )
+      .pipe( gulp.dest( "./_site/./" ) );
+    done();
+} );
+
+// Build the Jekyll Site
+gulp.task( "jekyll-build", done =>  {
+    browserSync.notify( messages.jekyllBuild );
+    return cp.spawn( jekyll, [ "build" ], { stdio: "inherit" } )
+        .on( "close", done );
+} );
+
+// Rebuild Jekyll & do page reload
+gulp.task( "rebuild",
+  gulp.series( [ "jekyll-build" ], done => {
+    browserSync.reload();
+    done();
+  } )
+);
+
+gulp.task( "clean", () => {
+  return gulp.src( "_site", { read: false, allowEmpty: true } )
+    .pipe( clean() );
+} );
+
+gulp.task( "serve", function() {
+  return browserSync( {
+    server: {
+      baseDir: "_site"
+    }
+  } );
+} );
+
+gulp.task( "styles", gulp.series( [ "sass", "critical" ] ) );
+
+gulp.task( "watch", () => {
+  gulp.watch( "_sass/**/*.scss", gulp.series( "styles" ) );
+  gulp.watch( [
+    "*.html",
+    "_layouts/*.html",
+    "_includes/*.html",
+    "pages_/*.md",
+    "_include/*html"
+  ], gulp.series( "rebuild" ) );
+  gulp.watch( "_js/**/*.js", gulp.series( "js" ) );
+} );
+
+gulp.task( "build", gulp.series( [
+  "clean",
+  gulp.parallel( [ "sass", "js" ] ),
+  "jekyll-build",
+  "critical"
+] ) );
+
+gulp.task( "default", gulp.series( [
+  "build",
+  "serve",
+  "watch"
+] ) );
---
+++ register.md
@@ -0,0 +1,49 @@
+---
+layout: page
+title: Register to Vote
+permalink: /register/
+---
+
+## Are you registered to vote in Mecklenburg County?
+
+You must be registered to vote before you can cast a ballot.
+
+To check your registration status, [click here](https://vt.ncsbe.gov/RegLkup/) and fill out the form to search for your registration. If you are not registered at your current address, continue reading to learn how you can still register before Election Day.
+
+If you are already registered, you can proceed to [preview your ballot](/ballot) or find your [polling place](/locations).
+
+## You can still register to vote during early voting!
+
+Rather than sending in your voter registration, you can register to vote and cast a ballot by using same-day registration.
+
+Same-day registration is available at one-stop voting sites throughout Mecklenburg County during early voting. You can not register to vote on Election Day.
+
+## Eligibility
+
+### Qualifications
+
+A prospective voter must meet all of the following qualifications to vote in North Carolina:
+
+* They must be a citizen of the United States.
+* They must live in the county of his/her registration, and have resided there for at least 30 days prior to the date of the election.
+* They must be at least 18 years old. A prospective voter can submit a registration form up to two years before his/her 18th birthday, if and only if he/she will be 18 at the time of the next general election.
+* They must not be serving a sentence for a felony conviction (including probation or parole). If a prospective voter has previously been convicted of a felony, his/her citizenship rights must be restored. For more information on voting rights for those in the North Carolina criminal justice system, click here. 
+
+This is just a summary of the qualifications. The [North Carolina State Board of Elections Website](https://www.ncsbe.gov/Voters/Registering-to-Vote) has more information including a more specific breakdown of the terms described above. [Click here](https://www.ncsbe.gov/Voters/Registering-to-Vote) to learn more.
+
+### Identifying Documents
+
+To register, you will need to bring an identifying document such as:
+
+* A North Carolina driver's license
+* A photo ID from a government agency
+* A student photo ID with a school document showing the student's address
+* A utility bill, bank statement, payroll stub, or document from any government agency with your name and current address.
+
+## Voter Registration Form
+
+Before you go, preview the voter registration form by [clicking here](https://dl.ncsbe.gov/Voter_Registration/NCVoterRegForm_06W.pdf) to be sure you have all of the information needed to register. You can also print and complete the document at home to make the process easier.
+
+## Find a One-Stop Voting Site!
+
+You can register vote and cast a [ballot](/ballot) at a one-stop voting site before election day. Find the site nearest to you by [clicking here](https://vt.ncsbe.gov/ossite/).
